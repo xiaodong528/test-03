5e999431f813d68b4fa3f4d7265c3226a12a9bb8
sxjcz <sxjczyd@163.com>
2023-03-03 21:06:31 +0800

    git-firstTest01

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * ProjectName: ${PROJECT_NAME}
+ * ClassName: ${NAME}
+ * Package: org.example
+ * Description:
+ *
+ * @Author ${USER}
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
